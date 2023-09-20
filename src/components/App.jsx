cc586c671aac605fdd0d06b2d9597035678db03b
Sagar Verma <105728547+Optimus5734@users.noreply.github.com>
2023-09-20 14:09:18 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+  function sub(event) {
+    const { value, name } = event.target;
+
+    if (name === "fName") {
+      setContact((prevValue) => {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      });
+    } else if (name === "lName") {
+      setContact((prevValue) => {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        };
+      });
+    } else {
+      setContact((prevValue) => {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        };
+      });
+    }
+  }
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={sub}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={sub}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={sub}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
